dd3e20d7d208002adc640db402b63a0e8b1aeaef
Aurimas <saauris@gmail.com>
2026-04-30 18:13:28 +0300

    Initial commit: backend and frontend

---
+++ flashcards-service/src/main/java/com/flashcards/flashcards_service/FlashcardsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flashcards.flashcards_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsServiceApplication.class, args);
+	}
+
+}
